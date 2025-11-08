6f4d81c0b656d092462dd9e085872ac90b1772fc
Francisco Mesa <francisco.mc.ids@outlook.com>
2025-11-08 17:26:39 -0400

    Update SVG icons

--- client/src/AppPage.tsx
+++ client/src/AppPage.tsx
@@ -103,14 +103,15 @@ export default function AppPage() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                className="stroke-info h-6 w-6 shrink-0"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
+                  d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                />
               </svg>
               <span>No items saved yet. Start by adding a note or link!</span>
             </div>
@@ -162,7 +163,7 @@ export default function AppPage() {
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
-                          d="M12 4.5v15m7.5-7.5h-15"
+                          d="M6 18 18 6M6 6l12 12"
                         />
                       </svg>
                     </button>
